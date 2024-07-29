99ee5433ef8c426bfc96ae3fc826bad7d0c1d62c
Temuulen8 <b.temuulen2005@gmail.com>
2024-07-29 08:43:23 +0800

    a

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,39 @@
+import Label from "../label";
+import { MdOutlineEmail } from "react-icons/md";
+import { TbBoxMultiple } from "react-icons/tb";
+import { LuPhone } from "react-icons/lu";
+import Heroicon from "../herosectionf/heroicon";
+
+const { default: Tag } = require("../tag")
+
+const Footer = () => {
+    return (
+        <footer className="container m-auto flex flex-col justify-center items-center gap-4 p-16 dark:text-white">
+            <Tag text = "Get in touch" />
+            <p>
+                What’s next? Feel free to reach out to me if you're looking for a
+                developer, have a query, or simply want to connect.
+            </p>
+            <div className="flex gap-8  items-center text-4xl font-semibold">
+                <MdOutlineEmail />
+                <p>b.temuulen2005@gmail.com</p>
+                <TbBoxMultiple />
+            </div>
+            <div className="flex gap-8  items-center text-4xl font-semibold">
+                <LuPhone />
+                <p>+976 88615033</p>
+                <TbBoxMultiple />
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-center">
+                <p>You may also find me on these platforms!</p>
+                <Heroicon />
+            </div>
+            <div className="flex gap-4 items-center p-6 justify-center bg-gray-50 dark:bg-[#111827] dark:text-white">
+            <FaRegCopyright />
+            <p>2024 | Greetings with ❤️️ from Ulaanbaatar</p>
+            </div>
+
+
+        </footer>
+    )
+}
